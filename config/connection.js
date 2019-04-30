f244fcc8abff3da051c146ed8c432726f060b779
Tori <victoria.fafrak@gmail.com>
2019-04-30 13:11:03 -0400

    attempt at heroku deployment with JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,24 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "us-cdbr-iron-east-02.cleardb.net",
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+  host: "localhost",
   port: 3306,
-  user: "b3b0c71b7e8440",
-  password: "8bccd8dd",
-  database: "heroku_1a3abefeca58a10"
+  user: "trilogy",
+  password: "password123",
+  database: "burgers_db"
 });
+}
+
+// var connection = mysql.createConnection({
+//   host: "us-cdbr-iron-east-02.cleardb.net",
+//   port: 3306,
+//   user: "b3b0c71b7e8440",
+//   password: "8bccd8dd",
+//   database: "heroku_1a3abefeca58a10"
+// });
 
 connection.connect(function(err) {
   if (err) {
