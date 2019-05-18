f49b051b004992524b4799a4a50f1a966817c10f
Tori <victoria.fafrak@gmail.com>
2019-05-18 11:22:14 -0400

    updated config

--- config/connection.js
+++ config/connection.js
@@ -12,14 +12,6 @@ if (process.env.JAWSDB_URL){
 });
 }
 
-// var connection = mysql.createConnection({
-//   host: "us-cdbr-iron-east-02.cleardb.net",
-//   port: 3306,
-//   user: "b3b0c71b7e8440",
-//   password: "8bccd8dd",
-//   database: "heroku_1a3abefeca58a10"
-// });
-
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
